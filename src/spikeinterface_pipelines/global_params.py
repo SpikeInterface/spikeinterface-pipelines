ef5340c0d2bd0807b413f7da9e639c304334694f
luiz <luiz_tauffer@hotmail.com>
2024-01-16 16:35:45 +0100

    fix typing

--- src/spikeinterface_pipelines/global_params.py
+++ src/spikeinterface_pipelines/global_params.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel, Field
+from typing import Union
 
 
 class JobKwargs(BaseModel):
-    n_jobs: int = Field(default=-1, description="The number of jobs to run in parallel.")
+    n_jobs: Union[int, float] = Field(default=-1, description="The number of jobs to run in parallel.")
     chunk_duration: str = Field(default="1s", description="The duration of the chunks to process.")
     progress_bar: bool = Field(default=False, description="Whether to display a progress bar.")
