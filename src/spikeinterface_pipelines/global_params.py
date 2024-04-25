b7472a519dc78ec7d1bcd3cc5d0a750eb490e990
luiz <luiz_tauffer@hotmail.com>
2024-04-25 18:15:59 +0200

    mp context

--- src/spikeinterface_pipelines/global_params.py
+++ src/spikeinterface_pipelines/global_params.py
@@ -6,3 +6,4 @@ class JobKwargs(BaseModel):
     n_jobs: Union[int, float] = Field(default=-1, description="The number of jobs to run in parallel.")
     chunk_duration: str = Field(default="1s", description="The duration of the chunks to process.")
     progress_bar: bool = Field(default=False, description="Whether to display a progress bar.")
+    mp_context: str = Field(default="spawn", description='Context for multiprocessing. It can be "fork" or "spawn".')
